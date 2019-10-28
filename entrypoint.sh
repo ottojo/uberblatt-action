9a47b600124f638f586aee8f920f99686d14e4c1
Jonas Otto (ottojo) <jottosmail@gmail.com>
2019-10-28 21:59:38 +0100

    add build script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-pwd
-ls -lah
+REPO_ROOT="$(pwd)"
+OUT_DIR="/tmp/build"
+
+for directory in */; do
+    echo "Building $directory"
+    cd $directory
+    mkdir -p $OUT_DIR/$directory
+    latexmk -pdf -output-directory=$OUT_DIR/$directory
+    # Remove every output that isn't a PDF
+    find $OUT_DIR/$directory -type f ! -name "*.pdf" -exec rm {} \;
+    cd ..
+done
